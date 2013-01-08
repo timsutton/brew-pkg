620a827f2e8034eea47685629567655ba9ed41a9
Timothy Sutton <tim@synthist.net>
2013-01-08 17:06:37 -0500

    brew-pkg.rb, initial commit

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-brew-pkg
-========
+# brew-pkg
 
-Build OS X installer packages directly from Homebrew formulae
+brew-pkg is a Homebrew external command that builds an OS X installer package from a formula. The formula must first already be installed on the system.
+
+## Usage
+
+Assuming nginx is already installed:
+
+`brew pkg nginx`
+<code><pre>==> Creating package staging root using Homebrew prefix /usr/local
+==> Staging formula nginx
+==> Plist found at homebrew.mxcl.nginx, staging for /Library/LaunchDaemons/homebrew.mxcl.nginx.plist
+==> Building package nginx-1.2.6.pkg</pre></code>
+
+It can also automatically include the formula's dependencies:
+
+`brew pkg --with-deps ffmpeg`
+<code><pre>==> Creating package staging root using Homebrew prefix /usr/local
+==> Staging formula ffmpeg
+==> Staging formula pkg-config
+==> Staging formula texi2html
+==> Staging formula yasm
+==> Staging formula x264
+==> Staging formula faac
+==> Staging formula lame
+==> Staging formula xvid
+==> Building package ffmpeg-1.1.pkg</pre></code>
+
+## Installing it
+
+Git clone the repo and symlink `brew-pkg.rb` in Homebrew's `Library/Contributions/cmds` directory.
+<code><pre>git clone https://github.com/timsutton/brew-pkg
+cd brew-pkg
+ln -s $(pwd)/brew-pkg.rb /usr/local/Library/Contributions/cmds/brew-pkg.rb</pre></code>
+
+Maybe later this will be a simple `brew install brew-pkg`.
+
+## Extras
+
+If a formula has defined a launchd plist, brew-pkg will also install this to the package's root in `/Library/LaunchDaemons`.
+
+You can also define a custom identifier prefix in the reverse-domain convention with the `--identifier-prefix` option, ie. `brew pkg --identifier-prefix org.nagios nrpe`. If there is a launchd plist defined, this same prefix is currently _not_ applied to the plist.
---
+++ brew-pkg.rb
@@ -0,0 +1,93 @@
+# Builds an OS X installer package from an installed formula.
+require 'formula'
+require 'optparse'
+require 'tmpdir'
+
+module HomebrewArgvExtension extend self
+  def with_deps?
+    flag? '--with-deps'
+  end
+end
+
+# cribbed Homebrew module code from brew-unpack.rb
+module Homebrew extend self
+  def pkg
+    unpack_usage = <<-EOS
+Usage: brew pkg [--identifier-prefix] [--with-deps] formula
+
+Build an OS X installer package from a formula. It must be already
+installed; 'brew pkg' doesn't handle this for you automatically. The
+'--identifier-prefix' option is strongly recommended in order to follow
+the conventions of OS X installer packages.
+
+Options:
+  --identifier-prefix     set a custom identifier prefix to be prepended
+                          to the built package's identifier, ie. 'org.nagios'
+                          makes a package identifier called 'org.nagios.nrpe'
+  --with-deps             include all the package's dependencies in the built package
+    EOS
+
+    abort unpack_usage if ARGV.empty?
+    identifier_prefix = if ARGV.include? '--identifier-prefix'
+      ARGV.next
+    else
+      'org.homebrew'
+    end
+
+    f = Formula.factory ARGV.last
+    # raise FormulaUnspecifiedError if formulae.empty?
+    # formulae.each do |f|
+    name = f.name
+    identifier = identifier_prefix + ".#{name}"
+    version = f.version.to_s
+
+    # Make sure it's installed first
+    if not f.installed?
+      onoe "#{f.name} is not installed. First install it with 'brew install #{f.name}'."
+      abort
+    end
+
+    # Setup staging dir
+    pkg_root = Dir.mktmpdir 'brew-pkg'
+    staging_root = pkg_root + HOMEBREW_PREFIX
+    ohai "Creating package staging root using Homebrew prefix #{HOMEBREW_PREFIX}"
+    FileUtils.mkdir_p staging_root
+
+    # Add deps if we specified --with-deps
+    pkgs = [f]
+    if ARGV.with_deps?
+      deps = f.deps.collect { |d| Formula.factory d.to_s }
+      pkgs += deps
+    end
+
+    pkgs.each do |pkg|
+      ohai "Staging formula #{pkg.name}"
+      # Get all directories for this keg, rsync to the staging root
+      dirs = Pathname.new(File.join(HOMEBREW_CELLAR, pkg.name, pkg.version.to_s)).children.select { |c| c.directory? }.collect { |p| p.to_s }
+      dirs.each {|d| safe_system "rsync", "-a", "#{d}", "#{staging_root}/" }
+
+      # Write out a LaunchDaemon plist if we have one
+      unless pkg.plist.nil?
+        ohai "Plist found at #{pkg.plist_name}, staging for /Library/LaunchDaemons/#{pkg.plist_name}.plist"
+        launch_daemon_dir = File.join staging_root, "Library", "LaunchDaemons"
+        FileUtils.mkdir_p launch_daemon_dir
+        fd = File.new(File.join(launch_daemon_dir, "#{pkg.plist_name}.plist"), "w")
+        fd.write pkg.plist
+        fd.close
+      end
+    end
+
+    # Build it
+    pkgfile = "#{name}-#{version}.pkg"
+    ohai "Building package #{pkgfile}"
+    safe_system "pkgbuild", \
+                "--quiet", \
+                "--root", "#{pkg_root}", \
+                "--identifier", identifier, \
+                "--version", version, \
+                "#{pkgfile}"
+    FileUtils.rm_rf pkg_root
+  end
+end
+
+Homebrew.pkg
