3f00d35e5aa7a23b928ec21208e358d52502c685
Ross Williams <ross@ross-williams.net>
2022-01-27 13:12:09 -0500

    Fix ARGV.next method not found
    Already partially addressed in timsutton/brew-pkg#16, but that
    PR missed one call to the non-existent `Array.next`.

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -31,7 +31,7 @@ Options:
 
     abort unpack_usage if ARGV.empty?
     identifier_prefix = if ARGV.include? '--identifier-prefix'
-      ARGV.next.chomp(".")
+      ARGV[ARGV.index('--identifier-prefix') + 1].chomp(".")
     else
       'org.homebrew'
     end
@@ -103,7 +103,7 @@ Options:
     # Add scripts if we specified --scripts 
     found_scripts = false
     if ARGV.include? '--scripts'
-      scripts_path = ARGV.next
+      scripts_path = ARGV[ARGV.index('--scripts') + 1]
       if File.directory?(scripts_path)
         pre = File.join(scripts_path,"preinstall")
         post = File.join(scripts_path,"postinstall")
@@ -126,7 +126,7 @@ Options:
     # Custom ownership
     found_ownership = false
     if ARGV.include? '--ownership'
-      custom_ownership = ARGV.next
+      custom_ownership = ARGV[ARGV.index('--ownership') + 1]
        if ['recommended', 'preserve', 'preserve-other'].include? custom_ownership
         found_ownership = true
         ohai "Setting pkgbuild option --ownership with value #{custom_ownership}"
